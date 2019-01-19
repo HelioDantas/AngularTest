b82929144f2e17e3e51518c27d8ef0d841df3235
helio <helio_dantas@outlook.com>
2019-01-18 23:42:40 -0200

    Rotas Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { CidadeModule } from './cidade/cidade.module';
+import { CidadesListaComponent } from './cidade/cidades-lista/cidades-lista.component';
 
+  const routes: Routes = [
+    { path: '',   redirectTo: '/cidades', pathMatch: 'full' },
+    {path:'cidades', component: CidadesListaComponent},
+    {path:'form', component: FormComponent}
+
+  ]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +22,8 @@ import { CidadeModule } from './cidade/cidade.module';
   imports: [
     BrowserModule,
     FormsModule,
-    CidadeModule
+    CidadeModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
